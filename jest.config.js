09be2433df2ddbee6bcc5e351048d33f06602c29
kghugo <howtostudycantonese@gmail.com>
2021-03-26 16:54:41 +0800

    Add test for services

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig.json');
+
 module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
@@ -9,5 +12,6 @@ module.exports = {
         ignoreCodes: [2451, 6133, 6192]
       }
 		}
-	}
+	},
+	modulePaths: ["node_modules", "."]
 }
